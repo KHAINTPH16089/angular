57ce7f0193407a77aa4711676e89c25757a17890
khaintph16089 <khaintph16089@fpt.edu.vn>
2022-06-28 17:36:37 +0700

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 const apiUrl = 'http://localhost:3001/api';
 
 export const environment = {
-  production: false,
+  production: true,
 
   product: `${apiUrl}/products`,
   category: `${apiUrl}/category`,
