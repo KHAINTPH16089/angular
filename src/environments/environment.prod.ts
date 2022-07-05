1f29b457f10bc609c1f20d68ceb232d9fa29c7f4
khaintph16089 <khaintph16089@fpt.edu.vn>
2022-07-05 15:59:20 +0700

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+const apiUrl = 'https://nodejs-angular.vercel.app/api';
 export const environment = {
-  
+  production: true,
+  apiUrl: 'https://nodejs-angular.vercel.app/api',
+  product: `${apiUrl}/products`,
+  category: `${apiUrl}/category`,
+  signup: `${apiUrl}/signup`,
+  signin: `${apiUrl}/signin`,
+  user: `${apiUrl}/user`,
+  order: `${apiUrl}/order`,
+  orderDetail: `${apiUrl}/orderDetail`,
+  upload: `${apiUrl}/upload`,
+  status: `${apiUrl}/status`,
 };
